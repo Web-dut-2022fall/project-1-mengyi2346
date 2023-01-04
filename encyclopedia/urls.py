6e4184c779b18d4b9af0ff57cb15e96710ad16da
mengyi2346 <118503753+mengyi2346@users.noreply.github.com>
2023-01-04 10:14:09 +0800

    Add files via upload

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<str:text_title>", views.text, name="text"),
+    path("add/", views.add, name="add"),
+    path("add/create", views.create, name="create"),
+    path("search/", views.search, name="search"),
+    path("rand/", views.rand, name="random"),
+    path("edit/<str:text_title>", views.edit, name="editP")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,10 @@
+import random
+
+from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render
+import markdown2
+
+from django.urls import reverse
 
 from . import util
 
@@ -8,3 +14,49 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def text(request, text_title):
+    content = util.get_entry(text_title)
+    if content is None:
+        raise Http404("Dose not exist.")
+
+    return render(request, "encyclopedia/text.html", {
+        "text_title": text_title,
+        "content": markdown2.markdown(content)
+    })
+
+
+def search(request):
+    q = request.POST.get("q")
+    return HttpResponseRedirect(reverse("text", args=(q,)))
+
+
+def add(request):
+    return render(request, "encyclopedia/add.html")
+
+
+def create(request):
+    title = request.POST.get("title")
+    content = "#" + request.POST.get("title") + '\n' + request.POST.get("text_content")
+    util.save_entry(title, content)
+    return HttpResponseRedirect(reverse("text", args=(title,)))
+
+
+def rand(request):
+    lis = util.list_entries()
+    target = random.choice(lis)
+    content = util.get_entry(target)
+    return render(request, "encyclopedia/text.html", {
+        "text_title": target,
+        "content": markdown2.markdown(content)
+    })
+
+
+def edit(request, text_title):
+    content = util.get_entry(text_title)
+    if content is None:
+        raise Http404("Dose not exist.")
+    return render(request, "encyclopedia/edit.html", {
+        "text_title": text_title,
+        "content": content
+    })
